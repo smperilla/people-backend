0cee17fddfbb6c56169a4f1aef0426f63c3acd42
sperilla <smperilla@gmail.com>
2023-09-16 12:56:11 -0400

    adding in cors and express.json

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const app = express();
 const PORT = 8000;
 const peopleController = require("./controllers/peopleController");
 const morgan = require("morgan");
+const cors = require("cors");
 
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the People App!");
-})
-
-
+});
 
 app.use("/people", peopleController);
 
